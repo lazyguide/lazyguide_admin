bfa9031d2e87b03764fe4f39582630db5c142551
tsaiyichen <easonjessiejj@gmail.com>
2024-02-27 16:48:52 +0800

    2024/02/27 change to alert

--- actVarify.php
+++ actVarify.php
@@ -18,35 +18,26 @@ if($type == "indoor"){
 <script>
     var name = "<?php echo $name ?>"; // 将 PHP 变量 $name 赋值给 JavaScript 变量
     var actID = <?php echo $actID ?>; // 将 PHP 变量 $actID 赋值给 JavaScript 变量
-    var type = "<?php echo $type ?>"; // 将 PHP 变量 $type 赋值给 JavaScript 变量
-
-    // 显示确认提示框
-    if(confirm("確定審核此活動？ 活動名稱：" + name)){
-        <?php
-        if ($type == "indoor"){
-        $sql2 = "UPDATE indoor_activity SET ISVARIFIED = 1 WHERE IDR_ACTID = $actID";
-        $result = mysqli_query($link, $sql2);
-        if($result){
-        ?>
-        location.href = "indoor.php";
-        <?php
-        }else{
-        echo "error";
-    }
-        }else{
-        $sql2 = "UPDATE outdoor_activity SET ISVARIFIED = 1 WHERE ODR_ACTID = $actID";
-        $result = mysqli_query($link, $sql2);
-        if($result){
-        ?>
-        location.href = "outdoor.php";
-        <?php
-        }else{
-        echo "error";
-    }
-        }
-        ?>
-    }else{
-        history.back();
-    }
+    var type = "<?php echo $type ?>";
+    alert("審核通過 活動名稱：" + name)
 </script>
+        <?php
+        if ($type == "indoor") {
+            $sql2 = "UPDATE indoor_activity SET ISVARIFIED = 1 WHERE IDR_ACTID = $actID";
+            $result = mysqli_query($link, $sql2);
+            if ($result) {
+                header("Location: indoor.php");
+            } else {
+                echo "error";
+            }
+        }else {
+            $sql2 = "UPDATE outdoor_activity SET ISVARIFIED = 1 WHERE ODR_ACTID = $actID";
+            $result = mysqli_query($link, $sql2);
+            if ($result) {
+                header("Location: outdoor.php");
+            } else {
+                echo "error";
+            }
+        }
+     ?>
 
