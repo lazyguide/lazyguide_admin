581f04c950de59540ac6d3c12ac6d5e2fe441244
tsaiyichen <easonjessiejj@gmail.com>
2024-02-27 17:02:37 +0800

    2024/02/27 delete alert

--- actDelete.php
+++ actDelete.php
@@ -14,33 +14,26 @@ if($type == "indoor"){
     $row = mysqli_fetch_assoc($result);
     $name = $row["ODR_ACTNAME"];
 }
-?>
-<script language="javascript">
-    if(confirm("確定刪除此活動？ 活動名稱：<?php echo $name?>")){
-        <?php
         if ($type == "indoor"){
         $sql2 = "DELETE FROM indoor_activity WHERE IDR_ACTID = $actID";
         $result = mysqli_query($link, $sql2);
         if($result){
-        ?>
-        location.href("indoor.php");<?php
+        ?><script>
+            alert("已刪除活動：<?php echo $name;?>");
+            location.href("indoor.php");</script><?php
         }else{
             echo "error";
         }
-
         }else{
         $sql2 = "DELETE FROM outdoor_activity WHERE ODR_ACTID = $actID";
         $result = mysqli_query($link, $sql2);
         if($result){
-        ?>
-        location.href("outdoor.php");<?php
+        ?><script>
+            alert("已刪除活動: <?php echo $name;?>");
+            location.href("outdoor.php");
+        </script><?php
         }else{
             echo "error";
         }
 
-        }?>
-
-    }else{
-        history.back();
-    }
-</script>
+        }
