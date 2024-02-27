049f259fe0c23adc390ed483e9e67d4d53106c09
tsaiyichen <easonjessiejj@gmail.com>
2024-02-27 16:27:20 +0800

    2024/02/27 js if else

--- actVarify.php
+++ actVarify.php
@@ -15,30 +15,40 @@ if($type == "indoor"){
     $name = $row["ODR_ACTNAME"];
 }
 ?>
-<script language="javascript">
-    if(confirm("確定審核此活動？ 活動名稱：<?php echo $name?>")) {
+<script>
+    var name = "<?php echo $name ?>"; // 将 PHP 变量 $name 赋值给 JavaScript 变量
+    var actID = <?php echo $actID ?>; // 将 PHP 变量 $actID 赋值给 JavaScript 变量
+    var type = "<?php echo $type ?>"; // 将 PHP 变量 $type 赋值给 JavaScript 变量
+
+    // 显示确认提示框
+    if(confirm("確定審核此活動？ 活動名稱：" + name)){
         <?php
-        if ($type == "indoor") {
-            $sql2 = "UPDATE indoor_activity SET ISVARIFIED = 1 WHERE IDR_ACTID = $actID";
-            $result = mysqli_query($link, $sql2);
-            if (!($result)) {
-                echo "error";
-            }
-        } else {
-            $sql = "UPDATE outdoor_activity SET ISVARIFIED = 1 WHERE ODR_ACTID = $actID";
-            $result = mysqli_query($link, $sql2);
-            if (!($result)) {
-                echo "error";
-            }
-        }?>
-        if(<?php echo $type;?> == "indoor"){
-            location.href="indoor.php";
-        }
-        else{
-            location.href="outdoor.php";
-        }
+        if ($type == "indoor"){
+        $sql2 = "UPDATE indoor_activity SET ISVARIFIED = 1 WHERE IDR_ACTID = $actID";
+        $result = mysqli_query($link, $sql2);
+        if($result){
+        ?>
+        // 跳转到 indoor.php
+        location.href = "indoor.php";
+        <?php
+        }else{
+        echo "error";
     }
-    else{
+        }else{
+        $sql2 = "UPDATE outdoor_activity SET ISVARIFIED = 1 WHERE ODR_ACTID = $actID";
+        $result = mysqli_query($link, $sql2);
+        if($result){
+        ?>
+        // 跳转到 outdoor.php
+        location.href = "outdoor.php";
+        <?php
+        }else{
+        echo "error";
+    }
+        }
+        ?>
+    }else{
         history.back();
     }
 </script>
+
