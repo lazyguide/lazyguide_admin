fdf2850db1cf3db81f59c4a38ab592b30f9a758a
tsaiyichen <easonjessiejj@gmail.com>
2024-02-27 16:50:35 +0800

    2024/02/27 delete js

--- actVarify.php
+++ actVarify.php
@@ -14,14 +14,6 @@ if($type == "indoor"){
     $row = mysqli_fetch_assoc($result);
     $name = $row["ODR_ACTNAME"];
 }
-?>
-<script>
-    var name = "<?php echo $name ?>"; // 将 PHP 变量 $name 赋值给 JavaScript 变量
-    var actID = <?php echo $actID ?>; // 将 PHP 变量 $actID 赋值给 JavaScript 变量
-    var type = "<?php echo $type ?>";
-    alert("審核通過 活動名稱：" + name)
-</script>
-        <?php
         if ($type == "indoor") {
             $sql2 = "UPDATE indoor_activity SET ISVARIFIED = 1 WHERE IDR_ACTID = $actID";
             $result = mysqli_query($link, $sql2);
